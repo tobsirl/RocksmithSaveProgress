599d20a652891237b67f13b71559dcf4c91bdadc
tobsirl <tobster111@gmail.com>
2017-03-06 18:52:51 +0000

    Changed the server call and db connection

---
+++ config/main.js
@@ -0,0 +1,11 @@
+//=====================================================================
+// Config file
+//=====================================================================
+
+module.exports = {  
+
+'database': 'mongodb://toby:99633426@ds157809.mlab.com:57809/rs_saveprogress',
+'port': process.env.PORT || 8000
+//'host': process.env.HOST || 'localhost';
+
+}
